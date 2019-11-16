f64249661f803599276c5643800904f945fa8d54
Chris Park <cpark16@gmail.com>
2019-11-15 19:08:14 -0800

    Update nav.php

--- public/nav.php
+++ public/nav.php
@@ -140,7 +140,7 @@ error_reporting(E_ALL ^ E_NOTICE);
                         <li><a href="hotdirs.php?<?php echo $urlvars; ?>&amp;maxdepth=<?php echo $maxdepth; ?>">Hot Dirs</a></li>
                         <li><a href="top50.php?<?php echo $urlvars; ?>">Top 50</a></li>
                         <li><a href="dupes.php?<?php echo $urlvars . '&mindupes=' . $mindupes; ?>">Dupes</a></li>
-                        <li><a href="hardlinks.php?<?php echo $urlvars . '&minhardlinks=' . $minhardlinks; ; ?>">Hardlinks</a></li>
+                        <li><a href="hardlinks.php?<?php echo $urlvars . '&minhardlinks=' . $minhardlinks; ?>">Hardlinks</a></li>
                         <li><a href="tags.php?<?php echo $urlvars; ?>">Tags</a></li>
                         <li><a href="timechange.php?<?php echo $urlvars; ?>">Time Change</a></li>
                         <li class="divider"></li>
